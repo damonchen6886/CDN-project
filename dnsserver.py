f1280cc6b519a8c7d8cf0d5aacc20d54f09e62eb
damonchen6886 <guchennn@gmail.com>
2020-12-14 11:55:39 -0500

    impletmented cache

--- dnsserver.py
+++ dnsserver.py
@@ -79,6 +79,7 @@ import socket
 import sys
 from find_host import get_min_ec2_loc
 from struct import pack, unpack
+import time
 #-p <port> -n <name>
 PORT = int(sys.argv[2])
 DOMAIN_NAME = str(sys.argv[4])
@@ -86,6 +87,8 @@ BUF_SIZE = 1024
 DNS_DOMAIN_NAME = ""
 ANS_END_IDX = 0
 TWO_EC2_IP = []
+CACHE = {}
+CACHE_TIME = 180
 def build_server():
     server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     # server.bind(("",PORT))
@@ -187,7 +190,9 @@ def create_socket_for_http(ec2_ip):
         print("test")
         rtt_raw = httpsocket.recv(BUF_SIZE).decode()
         print("result", rtt_raw)
+        print("**********************")
         index = rtt_raw.find("min/avg/max/stddev = ")
+        # if index != -1:
         start_index = index+ len("min/avg/max/stddev = ")
         rtt = rtt_raw[start_index,start_index+4]
         print("rtt parse", rtt)
@@ -213,6 +218,17 @@ def get_best_ec2_client():
         return TWO_EC2_IP[0]
 
 
+#update global variable cache
+def add2_cache(client_ip_addr, best_ec2_ip):
+    CACHE[client_ip_addr] = [best_ec2_ip,time.time()]
+
+
+# update time for specific ip address in cache
+def update_cache(client_ip_addr):
+    CACHE[client_ip_addr][1] = time.time()
+
+
+
 
 #  needs: best_ec2_ip  data[0](bytes)  address(host, port)
 def starter():
@@ -232,27 +248,37 @@ def starter():
         client_addr = data[1]
         # print(client_addr)
         client_ip_addr = data[1][0]
+        #############
+        if client_ip_addr in CACHE.keys():
+            if(time.time() - CACHE[client_ip_addr][1] < CACHE_TIME):
+                response_packet = pack_all(CACHE[client_ip_addr][0],packet)
+                server_socket.sendto(response_packet,client_addr)
+                #update cache
+                update_cache(client_ip_addr)
+                continue
+        ##############
         # print("running2")
         # header = unpack("!HHHHHH",packet[0:12])
         #get the best ec2 ip address
+       
         global TWO_EC2_IP
         TWO_EC2_IP = get_min_ec2_loc(client_ip_addr)
         print(TWO_EC2_IP)
-        # best_ec2_server = TWO_EC2_IP[0]
+        best_ec2_server = TWO_EC2_IP[0]
 
-        best_ec2_server = get_best_ec2_client()
-
+        # best_ec2_server = get_best_ec2_client()
+        #add to cache
+        add2_cache(client_ip_addr,best_ec2_server)
         # print("best")
         # print(best_ec2_ip)
         # pack the udp meesage  
         response_packet = pack_all(best_ec2_server,packet)
         
-       
+    
         if DNS_DOMAIN_NAME != DOMAIN_NAME:
             print("domain names are different")
             continue
         # send back
-
 
         server_socket.sendto(response_packet,client_addr)
        
